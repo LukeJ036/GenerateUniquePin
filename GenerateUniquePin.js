f248421bbdce6de8f4ed5d77c4857e3ca9739cdf
Luke Jennings <luke.jennings@036.co.uk>
2024-03-18 08:42:18 +0000

    Commenting out test

--- GenerateUniquePin.js
+++ GenerateUniquePin.js
@@ -30,11 +30,9 @@ function hasDuplicateDigits(code) {
 }
 
 function isSequentialNumber(code) {
-  // returns true is the number has sequential numbers
-  const digits = code.toString().split("");
-  return digits.every((digit, index, arr) => {
-    if (index === arr.length - 1) return true; // skip the last digit
-    return +arr[index + 1] === +digit + 1 || +arr[index + 1] === +digit - 1;
+  const digits = code.toString().split("").map(Number); // convert digits to numbers
+  return digits.slice(1).every((digit, index) => {
+    return digit === digits[index] + 1 || digit === digits[index] - 1;
   });
 }
 
